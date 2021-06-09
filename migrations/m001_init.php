d8678ec75535b87c27d2e63db5ba43bcc0edecde
r567tw <r567tw@gmail.com>
2021-06-09 13:08:54 +0800

    migration

--- migrations/m001_init.php
+++ migrations/m001_init.php
@@ -1,13 +1,26 @@
 <?php
-namespace app\migrations;
 
 class m001_init {
 
-    public function up(){
-
+    public function up()
+    {
+        $db = \app\core\Application::$app->db;
+        $SQL = "CREATE TABLE users (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                email VARCHAR(255) NOT NULL,
+                firstname VARCHAR(255) NOT NULL,
+                lastname VARCHAR(255) NOT NULL,
+                status TINYINT NOT NULL,
+                password VARCHAR(512) NOT NULL, 
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )  ENGINE=INNODB;";
+        $db->pdo->exec($SQL);
     }
 
-    public function down(){
-        
+    public function down()
+    {
+        $db = \thecodeholic\phpmvc\Application::$app->db;
+        $SQL = "DROP TABLE users;";
+        $db->pdo->exec($SQL);
     }
 }
